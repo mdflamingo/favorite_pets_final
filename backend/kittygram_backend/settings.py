6177218b5be88af634f134256607604e23c00264
Anastasiya <aiskoskina@yandex.ru>
2023-09-13 18:15:58 +0300

    Change settings.py

--- backend/kittygram_backend/settings.py
+++ backend/kittygram_backend/settings.py
@@ -8,6 +8,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # SECRET_KEY = os.getenv('SECRET_KEY', default='*')
 # DEBUG = os.getenv('DEBUG', default=False)
 # ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS', default='localhost 127.0.0.1').split()
+
+
+
 SECRET_KEY = 'django-insecure-cg6*%6d51ef8f#4!r3*$vmxm4)abgjw8mo!4y-q*uq1!4$-89$'
 DEBUG = False
 ALLOWED_HOSTS = [] # [158.160.69.232, 127.0.0.1, localhost, kotty.ddns.net]
